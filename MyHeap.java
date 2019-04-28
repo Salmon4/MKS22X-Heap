bde17230f9d7d4b89b7e61a83772ace929cd755a
Brandon Chen <bchen00@stuy.edu>
2019-04-28 00:19:35 -0400

    finished writing pushdown but still haven't tested it

--- MyHeap.java
+++ MyHeap.java
@@ -1,12 +1,48 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
+    //left
+    int left = (index * 2) + 1;
+    //right
+    int right = (index * 2) + 1;
+    if (size % 2 == 0 && left == size - 1){
+      if (data[index] < data[left]){
+        int temp = data[index];
+        data[index] = data[left];
+        data[left] = temp;
+        return;
+      }
+    }
+    else{
+      if (right < size && data[index] > data[left]){
+        int temp = data[index];
+        data[index] = data[left];
+        data[left] = temp;
+        pushDown(data,size,left);
+      }
+      else{
+        int temp = data[index];
+        data[index] = data[right];
+        data[right] = temp;
+      }
+      if (right < size && data[index] < data[right]){
+        int temp = data[index];
+        data[index] = data[left];
+        data[left] = temp;
+        pushDown(data,size,left);
+      }
+      else{
+        int temp = data[index];
+        data[index] = data[right];
+        data[right] = temp;
+      }
+    }
 
   }
 
   private static void pushUp(int[]data,int index){
 
   }
-  
+
   public static void heapify(int[]){
 
   }
